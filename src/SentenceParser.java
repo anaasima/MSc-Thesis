bbde1f728e204f0bd7679828c8eed3866268c49a
Ana-Maria <anamariasima9@gmail.com>
2024-11-29 20:33:03 +0100

    Improvements to declarative constraints

--- src/SentenceParser.java
+++ src/SentenceParser.java
@@ -19,13 +19,15 @@ public interface SentenceParser {
     void handlePreSequencePostSequence();
 
     /**
+     * Here we have some problems with the design of the model, look through notes to see what's it about
+     * !!! Here we must do Not Co-Existence between initial and end node and vice versa
      * After A ends, immediately start B and start C and start (D_or_E).
      * AlternateSuccession(A,B)
      * AlternateSuccession(A,C)
-     * AlternateSuccession(A,D_or_E)
-     * Not Co-Existence(D,E)
-     * Alternate Precedence(D,D_or_E)
-     * AlternatePrecedence(E,D_or_E)
+     * AlternatePrecedence(D,A)
+     * AlternatePrecedence(E,A)
+     * Co-Existence(B,C) - this one should be between all AND branches
+     * Not Co-Existence(D,E) - this one because they're part of an XOR
      * Exactly1(A)
      */
     void handlePreAndPostSequence();
@@ -41,12 +43,12 @@ public interface SentenceParser {
      * Exactly1(A)
      * AlternatePrecedence(B,A)
      * AlternatePrecedence(C,A)
-     * AlternatePrecedence(D_and_E,A)
+     * AlternatePrecedence(D,A)
+     * AlternatePrecedence(E,A)
      * Not Co-Existence(B,C)
-     * Not Co-Existence(B,D_and_E)
-     * Not Co-Existence(C,D_and_E)
-     * AlternateSuccession(D_and_E,D)
-     * AlternateSuccession(D_and_E,E)
+     * Not Co-Existence(B,D) // or instead of D any other branch in the AND group
+     * Not Co-Existence(C,D) // same as above
+     * Co-Existence(D,E)
      */
     void handlePreOrPostSequence();
 
@@ -57,47 +59,19 @@ public interface SentenceParser {
     void handePreEventuallyPostSequence();
 
     /**
-     * Don't like how this one turned out
-     * This one has problems
-     * <p>
-     *
+     * Same problem as in handlePreAndPostSequence
      * After A ends and B ends and (D_or_E) ends, immediately start C.
-     * Not Co-Existence(D,E)
      * AlternateSuccession(A,C)
      * AlternateSuccession(B,C)
-     * AlternateSuccession(D_or_E,C)
-     * Exactly1(D_or_E)
-     * AlternateResponse(D,D_or_E)
-     * AlternateResponse(E,D_or_E)
-     * End(C)
+     * AlternateResponse(D,C)
+     * AlternateResponse(E,C)
+     * Not Co-Existence(D,E)
+     * Exactly1(A) // weird but for some reason this makes the model good
      */
     void handlePreSequencePostAnd();
 
     /**
-     * Don't like how this one turned out
-     * The problem with this is that G_or_H appears more than once, even though We have the Exactly1(G_or_H). I think there is a problem with RuM
-     * <p>
-     *
-     * After A ends and B ends and (D_or_E) ends, immediately start C and start F and start (G_or_H).
-     * Exactly1(A)
-     * Exactly1(B)
-     * Exactly1(D_or_E)
-     * Exactly1(G_or_H)
-     * Not Co-Existence(D,E)
-     * AlternateResponse(D,D_or_E)
-     * AlternateResponse(E,D_or_E)
-     * AlternateSuccession(A,C)
-     * AlternateSuccession(B,C)
-     * AlternateSuccession(D_or_E,C)
-     * AlternateSuccession(A,F)
-     * AlternateSuccession(B,F)
-     * AlternateSuccession(D_or_E,F)
-     * AlternateSuccession(A,G_or_H)
-     * AlternateSuccession(B,G_or_H)
-     * AlternateSuccession(D_or_E,G_or_H)
-     * Not Co-Existence(G,H)
-     * AlternatePrecedence(G,G_or_H)
-     * AlternatePrecedence(H,G_or_H)
+     * Not going to implement anymore
      */
     void handlePreAndPostAnd();
 
@@ -108,39 +82,13 @@ public interface SentenceParser {
     void handlePreRepeatSincePostAnd();
 
     /**
-     * After A ends and B ends and (E_or_F) ends, immediately either start C or start D or start (G_and_H).
-     * AlternateSuccession(A,or)
-     * AlternateSuccession(B,or)
-     * AlternateSuccession(E_or_F,or)
-     * Not Co-Existence(E,F)
-     * AlternateResponse(E,E_or_F)
-     * AlternateResponse(F,E_or_F)
-     * Exactly1(E_or_F)
-     * Exactly1(A)
-     * Exactly1(B)
-     * AlternatePrecedence(C,or)
-     * AlternatePrecedence(D,or)
-     * AlternatePrecedence(G_and_H,or)
-     * Not Co-Existence(C,D)
-     * Not Co-Existence(C,G_and_H)
-     * Not Co-Existence(D,G_and_H)
-     * AlternateSuccession(G_and_H,G)
-     * AlternateSuccession(G_and_H,H)
+     * Not going to implement anymore
      */
     void handlePreOrPostAnd();
 
     /**
      * After A ends and B ends and (D_or_E) ends, eventually start C.
-     * Succession(A,C)
-     * Succession(B,C)
-     * Succession(D_or_E,C)
-     * Not Co-Existence(D,E)
-     * AlternateResponse(D,D_or_E)
-     * AlternateResponse(E,D_or_E)
-     * Exactly1(A)
-     * Exactly1(B)
-     * Exactly1(C)
-     * Exactly1(D_or_E)
+     * Same implementation as in handlePreSequencePostAnd
      */
     void handePreEventuallyPostAnd();
 
@@ -148,35 +96,19 @@ public interface SentenceParser {
      * After either A ends or B ends or (D_and_E) ends, immediately start C.
      * AlternateResponse(A,C)
      * AlternateResponse(B,C)
-     * AlternateResponse(D_and_E,C)
-     * Exactly1(C)
-     * End(C)
+     * AlternateResponse(D,C)
+     * AlternateResponse(E,C)
+     * Co-Existence(D,E)
      * Not Co-Existence(A,B)
-     * Not Co-Existence(A,D_and_E)
-     * Not Co-Existence(B,D_and_E)
-     * AlternateSuccession(D,D_and_E)
-     * AlternateSuccession(E,D_and_E)
+     * Not Co-Existence(A,D) // instead of D can be any other branch from the AND group
+     * Not Co-Existence(B,D) // same as above
+     * Exactly1(C)
+     * Not Chain Succession(initial node before A, B and D_and_E, C)
      */
     void handlePreSequencePostOr();
 
     /**
-     * After either A ends or B ends or (E_and_F) ends, immediately start C and start D and start (G_or_H).
-     * AlternateResponse(A,and)
-     * AlternateResponse(B,and)
-     * AlternateResponse(E_and_F,and)
-     * Not Co-Existence(A,B)
-     * Not Co-Existence(A,E_and_F)
-     * Not Co-Existence(B,E_and_F)
-     * AlternateSuccession(E,E_and_F)
-     * AlternateSuccession(F,E_and_F)
-     * Exactly1(and)
-     * Exactly1(G_or_H)
-     * AlternateSuccession(and,C)
-     * AlternateSuccession(and,D)
-     * AlternateSuccession(and,G_or_H)
-     * Not Co-Existence(G,H)
-     * AlternatePrecedence(G,G_or_H)
-     * AlternatePrecedence(H,G_or_H)
+     * Not going to implement anymore
      */
     void handlePreAndPostOr();
 
@@ -187,39 +119,13 @@ public interface SentenceParser {
     void handlePreRepeatSincePostOr();
 
     /**
-     * After either A ends or B ends or (E_and_F) ends, immediately either start C or start D or start (G_and_H).
-     * Not Co-Existence(A,B)
-     * Not Co-Existence(A,E_and_F)
-     * Not Co-Existence(B,E_and_F)
-     * Exactly1(or)
-     * AlternateResponse(A,or)
-     * AlternateResponse(B,or)
-     * AlternateResponse(E_and_F,or)
-     * AlternateSuccession(E,E_and_F)
-     * AlternateSuccession(F,E_and_F)
-     * Not Co-Existence(C,D)
-     * Not Co-Existence(C,G_and_H)
-     * Not Co-Existence(D,G_and_H)
-     * AlternatePrecedence(C,or)
-     * AlternatePrecedence(D,or)
-     * AlternatePrecedence(G_and_H,or)
-     * AlternateSuccession(G_and_H,G)
-     * AlternateSuccession(G_and_H,H)
+     * Not going to implement anymore
      */
     void handlePreOrPostOr();
 
     /**
      * After either A ends or B ends or (D_and_E) ends, eventually start C.
-     * AlternateResponse(A,C)
-     * AlternateResponse(B,C)
-     * AlternateResponse(D_and_E,C)
-     * Exactly1(C)
-     * End(C)
-     * Not Co-Existence(A,B)
-     * Not Co-Existence(A,D_and_E)
-     * Not Co-Existence(B,D_and_E)
-     * AlternateSuccession(D,D_and_E)
-     * AlternateSuccession(E,D_and_E)
+     * Same implementation as in handlePreEventuallyPostOr
      */
     void handePreEventuallyPostOr();
 
